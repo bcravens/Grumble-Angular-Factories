8fc22ba10114ca439ead922ae16da3fd3fa03619
Bradley Cravens <bradleycravens@gmail.com>
2016-11-21 18:54:20 -0500

    inital commit

--- js/app.js
+++ js/app.js
@@ -1,34 +1,65 @@
-"use strict";
-
-
-angular.module("grumblr", ["ui.router"])
+angular.module("grumblr", ["ui.router", "ngResource"])
   .config([
     "$stateProvider",
     RouterFunction
   ])
   .controller("GrumbleIndexController", [
+    "GrumbleFactory",
     GrumbleIndexControllerFunction
   ])
+  .controller("GrumbleShowController", [
+    "GrumbleFactory",
+    "$stateParams",
+    GrumbleShowControllerFunction
+  ])
+  .factory("GrumbleFactory", [
+    "$resource",
+    GrumbleFactoryCallback
+  ])
 
-function RouterFunction($stateProvider){
-  $stateProvider
-  .state("grumbleIndex", {
-    url: "/grumbles",
-    templateUrl: "js/ng-views/index.html",
-    controller: "GrumbleIndexController",
-    controllerAs: "GrumbleIndexViewModel"
-  })
-  .state("grumbleShow", {
-    url: "/grumbles/:id",
-    templateUrl: "js/ng-views/show.html"
-  });
-}
+  function GrumbleFactoryCallback($resource){
+    return $resource("http://localhost:3000/grumbles/:id", {}, {
+      update: {method: "PUT" }
+    });
+  };
 
-function GrumbleIndexControllerFunction(){
-  this.grumbles = [
-    {title: "Thesse"},
-    {title: "Are"},
-    {title: "Hardcoded"},
-    {title: "Grumbles"}
-  ]
-}
+  function RouterFunction($stateProvider){
+    $stateProvider
+    .state("grumbleIndex", {
+      url: "/grumbles",
+      templateUrl: "js/ng-views/index.html",
+      controller: "GrumbleIndexController",
+      controllerAs: "indexVm"
+    })
+    .state("grumbleShow", {
+      url: "/grumbles/:id",
+      templateUrl: "js/ng-views/show.html",
+      controller: "GrumbleShowController",
+      controllerAs: "showVm"
+    });
+  }
+
+  function GrumbleIndexControllerFunction(GrumbleFactory){
+    var indexVm = this;
+    indexVm.grumbles = GrumbleFactory.query();
+    indexVm.newGrumble = new GrumbleFactory();
+
+    indexVm.create = function($state){
+      indexVm.newGrumble.$save().then(function(res){
+        indexVm.grumbles.push(res)
+      })
+    };
+  }
+
+  function GrumbleShowControllerFunction(GrumbleFactory, $stateParams){
+    var showVm = this;
+    showVm.grumble = GrumbleFactory.get({id: $stateParams.id});
+
+    showVm.update = function() {
+      showVm.grumble.$update({id: $stateParams.id});
+    };
+
+    showVm.delete = function() {
+      showVm.grumble.$delete({id: $stateParams.id});
+    };
+  };
